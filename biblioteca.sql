bdd8ae111633701ea4e4bb68639d345d4f1d48db
romi-42069 <condorirominalq@gmail.com>
2024-11-07 18:59:52 -0300

    biblioteca.sql
    subi una nueva base de datos con los cambios de la tabla devoluciones y prestamos, solo toque eso

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2024 a las 22:26:22
+-- Tiempo de generación: 07-11-2024 a las 18:57:02
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -37,14 +37,14 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id_categoria`, `nombre`) VALUES
-(3, 'AutoAyuda'),
-(8, 'Fantasia'),
-(12, 'Romance'),
-(20, 'Biografia'),
-(21, 'Ciencia Ficcion'),
-(22, 'Cuentos'),
-(23, 'Novela'),
-(24, 'Terror');
+(1, 'AutoAyuda'),
+(2, 'Fantasia'),
+(3, 'Romance'),
+(4, 'Biografia'),
+(5, 'Ciencia Ficcion'),
+(6, 'Cuentos'),
+(7, 'Novela'),
+(8, 'Terror');
 
 -- --------------------------------------------------------
 
@@ -69,7 +69,7 @@ CREATE TABLE `comentarios` (
 
 CREATE TABLE `devoluciones` (
   `id_devolucion` int(11) NOT NULL,
-  `id_prestamo` int(11) DEFAULT NULL,
+  `id_prestamo` int(11) NOT NULL,
   `fecha_devolucion` date NOT NULL,
   `estado` enum('devuelto','deteriorado','perdido') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -79,10 +79,8 @@ CREATE TABLE `devoluciones` (
 --
 
 INSERT INTO `devoluciones` (`id_devolucion`, `id_prestamo`, `fecha_devolucion`, `estado`) VALUES
-(1, 3, '2024-11-03', 'devuelto'),
-(2, 4, '2024-11-03', 'devuelto'),
-(3, 2, '2024-11-03', 'deteriorado'),
-(5, 7, '2024-11-04', 'deteriorado');
+(1, 1, '2024-11-07', 'devuelto'),
+(2, 2, '2024-11-07', 'deteriorado');
 
 -- --------------------------------------------------------
 
@@ -106,11 +104,10 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`id_libro`, `titulo`, `autor`, `editorial`, `anio_publicacion`, `isbn`, `imagen`, `id_categoria`) VALUES
-(2, 'Anabelle', 'Warren', 'Plumas', '1989', '9', '', 3),
-(11, 'Borges, vida y literatura', 'Alejandro Vaccaro', 'emecé', '2015', '1', 'uploads/BIOGRAFIA.jpg', 20),
-(12, 'El Hobbit', 'J. R. R. Tolkien', 'minotauro', '1990', '2', 'uploads/HOBBIT.jpg', 23),
-(13, 'Cuentos de la Selva', 'Horacio Quiroga', 'lea', '1987', '3', 'uploads/CUENTOS.jpg', 22),
-(14, 'Frankestein', 'Mary Shelley', 'Alma Pocket', '1990', '5', 'uploads/FRANKESTEIN.jpg', 21);
+(1, 'Anabelle', 'Warren', 'Plumas', '1989', '9', '', 1),
+(3, 'El Hobbit', 'J. R. R. Tolkien', 'minotauro', '1990', '2', 'uploads/HOBBIT.jpg', 7),
+(4, 'Cuentos de la Selva', 'Horacio Quiroga', 'lea', '1987', '3', 'uploads/CUENTOS.jpg', 6),
+(5, 'Frankestein', 'Mary Shelley', 'Alma Pocket', '1990', '5', 'uploads/FRANKESTEIN.jpg', 5);
 
 -- --------------------------------------------------------
 
@@ -142,30 +139,6 @@ CREATE TABLE `pedidos` (
   `fecha_pedido` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `pedidos`
---
-
-INSERT INTO `pedidos` (`id_pedido`, `id_usuario`, `nombre`, `correo`, `id_libro`, `fecha_pedido`) VALUES
-(1, 4, 'romina', 'correo@example.com', 2, '2024-11-03 16:56:12'),
-(2, 4, 'romina', 'correo@example.com', 2, '2024-11-03 17:24:27'),
-(3, 4, 'romina', 'correo@example.com', 13, '2024-11-03 21:13:59'),
-(4, 4, 'romina', 'correo@example.com', 11, '2024-11-04 10:45:45');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `perdidas`
---
-
-CREATE TABLE `perdidas` (
-  `id_perdida` int(11) NOT NULL,
-  `id_prestamo` int(11) DEFAULT NULL,
-  `fecha_perdida` date NOT NULL,
-  `monto_compensacion` decimal(5,2) NOT NULL,
-  `estado` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 -- --------------------------------------------------------
 
 --
@@ -174,8 +147,8 @@ CREATE TABLE `perdidas` (
 
 CREATE TABLE `prestamos` (
   `id_prestamo` int(11) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `id_libro` int(11) DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_libro` int(11) NOT NULL,
   `titulo` varchar(45) NOT NULL,
   `fecha_prestamo` date NOT NULL,
   `fecha_devolucion` date DEFAULT NULL,
@@ -187,16 +160,8 @@ CREATE TABLE `prestamos` (
 --
 
 INSERT INTO `prestamos` (`id_prestamo`, `id_usuario`, `id_libro`, `titulo`, `fecha_prestamo`, `fecha_devolucion`, `devuelto`) VALUES
-(1, 1, 2, '', '2024-11-02', '2024-11-15', 0),
-(2, 1, 2, '', '2024-11-02', '2024-11-15', 1),
-(3, 4, 2, 'Anabelle', '2024-11-03', NULL, 1),
-(4, 1, 2, '', '2024-11-03', '2024-11-15', 1),
-(5, 4, 2, '', '2024-11-04', '2024-11-19', 0),
-(6, 4, 2, '', '2024-11-04', '2024-11-19', 0),
-(7, 4, 13, '', '2024-11-04', '2024-11-19', 0),
-(8, 4, 13, '', '2024-11-04', '2024-11-19', 0),
-(9, 4, 2, '', '2024-11-04', '2024-11-19', 0),
-(10, 4, 11, '', '2024-11-04', '2024-11-19', 0);
+(1, 2, 3, '', '2024-11-07', '2024-11-22', 1),
+(2, 4, 4, '', '2024-11-07', '2024-11-22', 1);
 
 -- --------------------------------------------------------
 
@@ -206,14 +171,14 @@ INSERT INTO `prestamos` (`id_prestamo`, `id_usuario`, `id_libro`, `titulo`, `fec
 
 CREATE TABLE `roles` (
   `id_roles` int(11) NOT NULL,
-  `describcion` varchar(255) DEFAULT NULL
+  `descripcion` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `roles`
 --
 
-INSERT INTO `roles` (`id_roles`, `describcion`) VALUES
+INSERT INTO `roles` (`id_roles`, `descripcion`) VALUES
 (1, 'Administrador'),
 (2, 'Socio'),
 (3, 'Cliente');
@@ -240,8 +205,9 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `email`, `contra`, `telefono`, `id_roles`) VALUES
 (1, 'Ana Romina Liz', 'liz', 'anarominaliz123@gmail.com', 'e50b62248285bceb426d9c983866d05d09d68b95a841841e644b4063577ac38a', '123456789', 2),
-(4, 'romina', 'condori', 'anabisha346@gmail.com', '$2y$10$2V8aCosCDMTRLoXCzrTrJOKmNDsb6Of9Xs4qgEIxM.Evh5cPkD6jG', '1135894206', 3),
-(11, 'Ana Romina Liz', 'liz', 'Administrador@gmail.com', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', '123456789', 1);
+(2, 'romina', 'condori', 'anabisha346@gmail.com', '$2y$10$2V8aCosCDMTRLoXCzrTrJOKmNDsb6Of9Xs4qgEIxM.Evh5cPkD6jG', '1135894206', 3),
+(3, 'Ana Romina Liz', 'liz', 'Administrador@gmail.com', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', '123456789', 1),
+(4, 'Estefania', 'Montañez', 'estefiprueba@gmail.com', '$2y$10$gF84cyu/xAdI8/q2trp1O.V7T9UemWpKomJvcATbK.tsIoGkl00xC', '1134567893', 3);
 
 --
 -- Índices para tablas volcadas
@@ -273,7 +239,6 @@ ALTER TABLE `devoluciones`
 --
 ALTER TABLE `libros`
   ADD PRIMARY KEY (`id_libro`),
-  ADD UNIQUE KEY `isbn` (`isbn`),
   ADD KEY `id_categoria` (`id_categoria`);
 
 --
@@ -291,13 +256,6 @@ ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`id_pedido`),
   ADD KEY `id_usuario` (`id_usuario`),
   ADD KEY `id_libro` (`id_libro`);
-
---
--- Indices de la tabla `perdidas`
---
-ALTER TABLE `perdidas`
-  ADD PRIMARY KEY (`id_perdida`),
-  ADD KEY `id_prestamo` (`id_prestamo`);
 
 --
 -- Indices de la tabla `prestamos`
@@ -328,7 +286,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
@@ -340,13 +298,13 @@ ALTER TABLE `comentarios`
 -- AUTO_INCREMENT de la tabla `devoluciones`
 --
 ALTER TABLE `devoluciones`
-  MODIFY `id_devolucion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_devolucion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `multas`
@@ -358,25 +316,25 @@ ALTER TABLE `multas`
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `perdidas`
---
-ALTER TABLE `perdidas`
-  MODIFY `id_perdida` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
-  MODIFY `id_prestamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_prestamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_roles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -393,7 +351,7 @@ ALTER TABLE `comentarios`
 -- Filtros para la tabla `devoluciones`
 --
 ALTER TABLE `devoluciones`
-  ADD CONSTRAINT `devoluciones_ibfk_1` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamos` (`id_prestamo`);
+  ADD CONSTRAINT `devoluciones_ibfk_1` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamos` (`id_prestamo`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `libros`
@@ -416,17 +374,11 @@ ALTER TABLE `pedidos`
   ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`);
 
 --
--- Filtros para la tabla `perdidas`
---
-ALTER TABLE `perdidas`
-  ADD CONSTRAINT `perdidas_ibfk_1` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamos` (`id_prestamo`);
-
---
 -- Filtros para la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
-  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`);
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `usuarios`
