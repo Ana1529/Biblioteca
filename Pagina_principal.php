53b50ac3795791952a9595af7fa45d941e4bb86d
LizGutierrez <saidgoticohotmail@gmail.com>
2024-11-07 22:14:28 -0300

    css
    vincule el css

--- Pagina_principal.php
+++ Pagina_principal.php
@@ -36,7 +36,7 @@ $conexion->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
-    <link rel="stylesheet" href="CSS/pagina_principal.css">
+    <link rel="stylesheet" href="css/pagina_principal.css">
     <script src="java.js" defer></script> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
